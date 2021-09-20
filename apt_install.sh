e4e8b387f1f583bdd8eab035fc265db8819e8a49
Zhenbo Li <lizhenbo@yahoo.com>
2021-09-20 10:38:12 +0800

    cfg

--- apt_install.sh
+++ apt_install.sh
@@ -2,6 +2,7 @@
 sudo apt-get update -y && sudo apt-get upgrade -y
 
 sudo apt-get install python3 python3-dev   \
+libcurl4-openssl-dev \
 gir1.2-atk-1.0 gir1.2-atspi-2.0 gir1.2-gtk-3.0 gir1.2-harfbuzz-0.0 \
 gir1.2-pango-1.0 libasyncns0 libatk-bridge2.0-dev libatk1.0-dev \
 libatspi2.0-dev libdatrie-dev libdbus-glib-1-dev-bin libegl-dev libegl-mesa0  \
@@ -15,4 +16,5 @@ libxdamage-dev libxfixes-dev libxi-dev libxinerama-dev libxkbcommon-dev  \
 libxrandr-dev libxtst-dev pango1.0-tools wayland-protocols  \
 x11proto-input-dev x11proto-randr-dev x11proto-record-dev \
 x11proto-xinerama-dev libnode64 libossp-uuid16 nodejs-doc \
-libcurl4-openssl-dev
+libasound2-dev libdbus-glib-1-dev libdrm-dev libgtk-3-dev libpulse-dev libx11-xcb-dev \
+uuid nasm
