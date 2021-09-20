2eb234cc6143bebca364d42125899f7f1a5e7ddc
Zhenbo Li <lizhenbo@yahoo.com>
2021-09-20 09:20:02 +0800

    bash

--- apt_install.sh
+++ apt_install.sh
@@ -13,5 +13,5 @@ libpulse-mainloop-glib0 libpulse0 libsndfile1 libthai-dev libvorbisenc2  \
 libwayland-bin libwayland-dev libxcomposite-dev libxcursor-dev  \
 libxdamage-dev libxfixes-dev libxi-dev libxinerama-dev libxkbcommon-dev  \
 libxrandr-dev libxtst-dev pango1.0-tools wayland-protocols  \
-x11proto-input-dev x11proto-randr-dev x11proto-record-dev 
+x11proto-input-dev x11proto-randr-dev x11proto-record-dev \
 x11proto-xinerama-dev
