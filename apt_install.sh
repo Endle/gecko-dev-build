b3f44725b5a5b04ef4f608c8752199ebb0a98530
Zhenbo Li <lizhenbo@yahoo.com>
2021-09-20 09:05:39 +0800

    Separater file

---
+++ apt_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo apt-get update -y && sudo apt-get upgrade -y
+
+sudo apt-get install python3 python3-dev   \
+gir1.2-atk-1.0 gir1.2-atspi-2.0 gir1.2-gtk-3.0 gir1.2-harfbuzz-0.0 \
+gir1.2-pango-1.0 libasyncns0 libatk-bridge2.0-dev libatk1.0-dev \
+libatspi2.0-dev libdatrie-dev libdbus-glib-1-dev-bin libegl-dev libegl-mesa0  \
+libegl1 libegl1-mesa-dev libepoxy-dev libflac8 libfribidi-dev libgl-dev  \
+libgl1-mesa-dev libgles-dev libgles1 libgles2 libglvnd-dev libglx-dev  \
+libgraphite2-dev libharfbuzz-dev libharfbuzz-gobject0 libharfbuzz-icu0  \
+libopengl-dev libopengl0 libpango1.0-dev libpangoxft-1.0-0  \
+libpulse-mainloop-glib0 libpulse0 libsndfile1 libthai-dev libvorbisenc2  \
+libwayland-bin libwayland-dev libxcomposite-dev libxcursor-dev  \
+libxdamage-dev libxfixes-dev libxi-dev libxinerama-dev libxkbcommon-dev  \
+libxrandr-dev libxtst-dev pango1.0-tools wayland-protocols  \
+x11proto-input-dev x11proto-randr-dev x11proto-record-dev 
+x11proto-xinerama-dev
